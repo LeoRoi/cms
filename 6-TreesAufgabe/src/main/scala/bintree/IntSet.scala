69eb42cfed99ebfb346a38fd8f49e9213e0ae189
LeoRoi <kuzne4eg@gmail.com>
2019-01-05 11:53:47 +0100

    6

---
+++ 6-TreesAufgabe/src/main/scala/bintree/IntSet.scala
@@ -0,0 +1,147 @@
+package bintree
+
+
+/**
+  * tree construct
+  */
+abstract class IntSet {
+  def insert(x: Int): IntSet
+  def contains(x: Int): Boolean
+}
+
+case object Empty extends IntSet {
+  def contains(x: Int): Boolean = false
+  def insert(x: Int): IntSet = new NonEmpty(x, Empty, Empty)
+}
+
+case class NonEmpty(elem: Int, val left: IntSet, val right: IntSet) extends IntSet {
+  def contains(x: Int): Boolean =
+    if (x < elem) left contains x
+    else if (x > elem) right contains x
+    else true
+
+  def insert(x: Int): IntSet =
+    if (x < elem) NonEmpty(elem, left insert x, right)
+    else if (x > elem) NonEmpty(elem, left, right insert x)
+    else this
+}
+
+
+/**
+  tree operations
+  */
+object Aufgaben {
+  def findSuccessor(tree: IntSet): Option[Int] = {
+    def findMostLeftElem(tree: IntSet): Option[Int] = tree match {
+      case NonEmpty(value, Empty, _) => Option(value)
+      case NonEmpty(value, left, _) => findMostLeftElem(left)
+    }
+
+    tree match {
+      case NonEmpty(_, Empty, _) => None
+      case NonEmpty(_, _, right) => findMostLeftElem(right)
+    }
+  }
+
+  def delete(Elem: Int, tree: IntSet): IntSet = tree match {
+
+    // Fall 1: Elememt nicht gefunden
+    case Empty => Empty
+    // Fall 2: Element gefunden und rechter oder linker Teilbaum leer
+    case NonEmpty(Elem, Empty, right) => right
+    case NonEmpty(Elem, left, Empty) => left
+    // Fall 3: Knoten gefunden und ist in der Mitte des Baums
+    case NonEmpty(Elem, left, right) => {
+      val successor = findSuccessor(tree)
+      new NonEmpty(successor.get, left, delete(successor.get, right))
+    }
+    // Weiter Suchen....
+    case NonEmpty(value, left, right) => if (Elem < value) new NonEmpty(value, delete(Elem, left), right)
+    else new NonEmpty(value, left, delete(Elem, right))
+  }
+
+  // Funktion ueberfuehrt eine Liste von Zahlen in einen Binary Tree
+  def list2Tree(l: List[Int]): IntSet = {
+    def unrollList(list: List[Int], acc: IntSet): IntSet = list match {
+      case head :: tail => unrollList(tail, acc.insert(head))
+      case Nil => acc
+    }
+
+    if (l.size == 0)
+      Empty
+    else if (l.size == 1)
+      NonEmpty(l.head, Empty, Empty)
+    else
+      unrollList(l.tail, NonEmpty(l.head, Empty, Empty))
+  }
+
+  // Funktion ueberfuehrt einen Binaeren Suchbaum in eine sortierte Liste
+  def tree2SortedList(t: IntSet): List[Int] = {
+    def unrollTree(tree: IntSet, acc: List[Int], lastElement: Int): List[Int] = tree match {
+      case node: NonEmpty => {
+        val head = node.elem
+        if (acc.isEmpty) unrollTree(delete(head, tree), List(head), head)
+        else if (head >= lastElement) unrollTree(delete(head, tree), acc :+ head, head)
+        else unrollTree(delete(head, tree), List(head) ::: acc, head)
+      }
+      case Empty => acc
+    }
+
+    unrollTree(t, List(), 0)
+  }
+
+  // Funktion ueberfuerhrt einen Binaeren Suchbaum in eine Liste, in dem der
+  // Baum Ebene fuer Ebene durchlaufen wird
+  def breadthFirstSearch(tree: IntSet): List[Int] = {
+    def bfs(nodes: List[IntSet]): List[Int] = nodes match {
+      case Nil => List() //nil == empty list
+      case Empty :: tail => bfs(tail)
+      case NonEmpty(head, Empty, Empty) :: tail => head :: bfs(tail)
+      case NonEmpty(head, left: IntSet, right: IntSet) :: tail =>
+        head :: bfs(tail ++ List(left, right))
+      case _ => throw new Error("omg")
+    }
+    bfs(List(tree))
+  }
+
+  def printLevelOrder(tree: IntSet): List[Int] = {
+    var out = List[Int]()
+
+    def printGivenLevel (tree: IntSet, level: Int): Unit = tree match {
+      case node: NonEmpty => {
+        if (level == 1)
+//          print(node.elem + " ")
+          out :+ node.elem
+        else if (level > 1) {
+          printGivenLevel(node.left, level - 1)
+          printGivenLevel(node.right, level - 1)
+        }
+      }
+      case Empty =>
+    }
+    val h = height(tree)
+    for (i <- 1 to h+1) printGivenLevel(tree, i)
+    out
+  }
+
+  // https://www.geeksforgeeks.org/level-order-tree-traversal/
+  // Funktion bestimmt die Hoehe eines Binaeren Suchbaums
+  def height(tree: IntSet): Int = {
+    def traverse(acc: Int, tree: IntSet): Int = tree match {
+      case node: NonEmpty => {
+        val left = height(node.left)
+        val right = height(node.right)
+        if(left > right) left+1
+        else right+1
+      }
+      case Empty => acc
+    }
+    traverse(-1, tree)
+  }
+
+  def heightG(tree: IntSet): Int = tree match {
+    case Empty => -1
+    case NonEmpty(_, left, right) =>
+      (1+heightG(left)).max(1+heightG(right))
+  }
+}
---
+++ 6-TreesAufgabe/src/test/scala/test/BinTreeTests.scala
@@ -0,0 +1,102 @@
+package test
+
+import bintree._
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class Beleg1Tests extends FunSuite {
+  trait TestTrees {
+    val l = List(7, 3, 10, 15)
+    val t0 = NonEmpty(7, NonEmpty(3, Empty, Empty), NonEmpty(10, Empty, NonEmpty(15, Empty, Empty)))
+    val t1 = Aufgaben.list2Tree(List(3, 6, 2, 5, 9, 1))
+    val t2 = Aufgaben.list2Tree(List(3, 6, 2, 5, 9, 1, 2, 9))
+    val t3 = Aufgaben.list2Tree(List(3, 6, 2, 5, 9, 1, 4, 11))
+    val t4 = NonEmpty(7, NonEmpty(3, Empty, Empty), NonEmpty(10, Empty, NonEmpty(15, NonEmpty(1, Empty, Empty), Empty)))
+  }
+
+  test("List2Tree-Test") {
+    new TestTrees {
+      assert(t0 === Aufgaben.list2Tree(l))
+    }
+  }
+
+
+  test("Tree2sortedList") {
+    new TestTrees {
+      assert(List(1, 2, 3, 5, 6, 9) === Aufgaben.tree2SortedList(t1))
+    }
+  }
+
+  test("Binary Tree insert mit Doppelten") {
+    new TestTrees {
+      assert(List(1, 2, 3, 5, 6, 9) === Aufgaben.tree2SortedList(t2))
+    }
+  }
+
+  test("BreadtFirstSearchTest") {
+    new TestTrees {
+      assert(List(3, 2, 6, 1, 5, 9, 4, 11) === Aufgaben.breadthFirstSearch(t3))
+    }
+  }
+
+  test("HeightTest") {
+    new TestTrees {
+      assert(0 === Aufgaben.height(NonEmpty(9, Empty, Empty)))
+      assert(1 === Aufgaben.height(NonEmpty(9, NonEmpty(4, Empty, Empty), Empty)))
+      assert(2 === Aufgaben.height(t0))
+      assert(3 === Aufgaben.height(t4))
+    }
+  }
+
+  test("HeightG") {
+    new TestTrees {
+      assert(0 === Aufgaben.heightG(NonEmpty(9, Empty, Empty)))
+      assert(1 === Aufgaben.heightG(NonEmpty(9, NonEmpty(4, Empty, Empty), Empty)))
+      assert(2 === Aufgaben.heightG(t0))
+      assert(3 === Aufgaben.heightG(t4))
+    }
+  }
+
+  test("findSuccessor-Test") {
+
+    new TestTrees {
+
+      assert(Option(4) === Aufgaben.findSuccessor(t3))
+    }
+  }
+
+
+  test("Delete-Elem not found") {
+
+    new TestTrees {
+      val t = Aufgaben.delete(45, t3)
+      assert(Aufgaben.breadthFirstSearch(t3) === Aufgaben.breadthFirstSearch(t))
+    }
+  }
+
+  test("Delete-Elem Mitte") {
+
+    new TestTrees {
+      val t = Aufgaben.delete(3, t3)
+      assert(List(4, 2, 6, 1, 5, 9, 11) === Aufgaben.breadthFirstSearch(t))
+    }
+  }
+
+  test("Delete-Elem Blatt") {
+
+    new TestTrees {
+      val t = Aufgaben.delete(11, t3)
+      assert(List(3, 2, 6, 1, 5, 9, 4) === Aufgaben.breadthFirstSearch(t))
+    }
+  }
+
+//  test("BreadthFirstSearchTest2") {
+//    new TestTrees {
+//      val tree = NonEmpty(1, NonEmpty(2, NonEmpty(4, Empty, Empty), NonEmpty(5, Empty, Empty)), NonEmpty(3, Empty, Empty))
+//      assert(List(1,2,3,4,5) === Aufgaben.printLevelOrder(tree))
+//    }
+//  }
+}
